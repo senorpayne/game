96760cb03df1532934083b21dacd527b3fcad5c2
Jason Payne <senorpayne@yahoo.com>
2018-01-03 12:21:14 -0600

    onclick

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,23 @@ $(document).ready(function()
 
 {$("img").on("click",function(){
 
-alert("yes"); 
+alert("yes") }
 
 
-var random = Math.floor(Math.random() * 120) +19;
-$("#random").text(random);
+var randomNumber = Math.floor(Math.random() * 120) +19;
+
+
+$("#random").text(randomNumber);
 
 var test = confirm("hi");
 alert(test);
+$("#one").on("click",function(){alert("test");}
+randomNumber();
 
-	
-	
+
+
+
+
 
 
  
