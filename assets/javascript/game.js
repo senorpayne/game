57e856b7001ac2d25939e64ad19ea5b9a297b2cb
Jason Payne <senorpayne@yahoo.com>
2018-01-03 12:59:43 -0600

    fonts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function()
 
 {$("img").on("click",function(){
 
-alert("yes") }
+alert("yes");});}
 
 
 var randomNumber = Math.floor(Math.random() * 120) +19;
@@ -14,8 +14,8 @@ $("#random").text(randomNumber);
 
 var test = confirm("hi");
 alert(test);
-$("#one").on("click",function(){alert("test");}
-randomNumber();
+$("#one").on("click",function(){alert("test");})
+
 
 
 
