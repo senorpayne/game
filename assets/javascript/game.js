fd92484ca46db8b43ff73116683cb54186b822cf
Jason Payne <senorpayne@yahoo.com>
2018-01-03 22:07:06 -0600

    JQ

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,21 @@ $(document).ready(function()
 
 
 {$("#one").on("click",function(){
-$("#one").val(4)
+ var rand = Math.floor(Math.random()*4)+1;
+ $("#yourScore").text(rand);
 alert("yes");})
+
+
+
+
+
+
+
 
 var newDiv = $("<div>");
 newDiv.text("Good Luck!");
 $(".jumbotron").append(newDiv);
+
 
 
 
