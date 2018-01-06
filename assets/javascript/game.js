7ae32358c79c191781b77e357a3479c01fb9737a
Jason Payne <senorpayne@yahoo.com>
2018-01-05 18:48:02 -0600

    moreworkingstuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,38 @@ var score = 0;
 var target =0;
 var wins = 0;
 var losses = 0;
-
 $(document).ready(function()
- 
+
 
 {$("#one").on("click",function(){
  var rand0 = Math.floor(Math.random()*12)+1;
  score+=rand0; 
  $("#yourScore").text(score);
-
-alert("yes");})
+scorecheck();
+})
 
 
 $("#two").on("click",function(){
  var rand1 = Math.floor(Math.random()*12)+1;
  score+=rand1; 
  $("#yourScore").text(score);
-alert("yes");})
+ scorecheck();
+})
 
 
 $("#three").on("click",function(){
  var rand2 = Math.floor(Math.random()*12)+1;
  score+=rand2;
  $("#yourScore").text(score);
-alert("yes");})
+ scorecheck();
+})
 
 $("#four").on("click",function(){
  var rand3 = Math.floor(Math.random()*12)+1;
  score+=rand3;
  $("#yourScore").text(score);
-alert("yes");})
+ scorecheck();
+})
 
 
 var newDiv = $("<div>");
@@ -46,6 +48,22 @@ var randomNumber = Math.floor(Math.random() * 120) +19;
 
 $("#random").text(randomNumber);
 
+
+function scorecheck(){
+
+    if ( score > randomNumber)
+    alert("Game Over! Please Refresh and try again");
+    losses++
+
+    if (score === randomNumber)
+    alert("You Win. You have great luck!");
+
+}
+
+function win(){
+
+    
+}
 console.log("You won!");   
 
 
